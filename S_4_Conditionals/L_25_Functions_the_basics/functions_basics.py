f98e7f6fc67a97e817e10d87e471c6541b995e75
kswr <ksawery.oleszynski@gmail.com>
2018-02-05 15:56:38 +0100

    lecture 25

---
+++ S_4_Conditionals/L_25_Functions_the_basics/functions_basics.py
@@ -0,0 +1,17 @@
+# Functions - the Basics [25/4]
+
+def minutes_to_hours(minutes):
+    hours = minutes/60
+    return hours
+
+def seconds_to_hours(seconds):
+    hours = seconds/3600
+    return hours
+
+print(minutes_to_hours(30))
+print(seconds_to_hours(7200))
+
+def hello_world():
+    return("Hello world")
+
+print(hello_world())
