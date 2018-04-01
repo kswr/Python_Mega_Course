35d89a2a70cbac2e15dcb54e6e8e020c6951b3a7
kswr <ksawery.oleszynski@gmail.com>
2018-04-01 23:16:46 +0200

    tkinter for database app

---
+++ S_16_Desktop_database_app/app.py
@@ -0,0 +1,47 @@
+"""
+A program that stores book information:
+Title, Author, Year, ISBN
+
+User can:
+
+View all records
+Search all entry
+Add entry
+Update entry
+Delete entry
+Close app
+"""
+
+from tkinter import *
+
+window = Tk()
+
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l2 = Label(window, text="Author")
+l2.grid(row=0, column=2)
+
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
+
+title_text = StringVar()
+e1 = Entry(window, textvariable=title_text)
+e1.grid(row=0, column=1)
+
+author_text = StringVar()
+e2 = Entry(window, textvariable=author_text)
+e2.grid(row=0, column=3)
+
+year_text = StringVar()
+e3 = Entry(window, textvariable=year_text)
+e3.grid(row=1, column=1)
+
+ISBN_text = StringVar()
+e4 = Entry(window, textvariable=ISBN_text)
+e4.grid(row=1, column=3)
+
+window.mainloop()
